f23529d7c860eab671a190316bf70844ef23d507
jessicasewe <jessicasewe89@gmail.com>
2023-08-24 03:58:44 -0700

    new update

--- opcode2.c
+++ opcode2.c
@@ -2,17 +2,21 @@
 
 /**
  * _add - Function that adds the top two elements of the stack.
- * @head: Double pointer to the head of the stack
+ * @head: stack head
+ * @data: unsused parameter
  * @line_number: Line number of the current opcode
  */
 void _add(stack_t **head, unsigned int data)
 {
-	(void)head;
+	(void)data;
 
 	if (gs.size < 2)
-		myexit(-8, "add");
-	data = gs.tail->n;
-	mlist_remove(gs.tail);
-	data += gs.tail->n;
-	gs.tail->n = data;
+	{
+		fprintf(stderr, "L%d: cant add, stack too short\n", line_number)
+		myexit(-8, NULL);
+	}
+	int result = gs.tail->n + gs.tail->prev->n;
+
+	gs.tail->prev->n = result;
+	pop(head, data);
 }
