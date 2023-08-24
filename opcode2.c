2c100e54794c8a7113585e62c084a48d60e5711c
jessicasewe <jessicasewe89@gmail.com>
2023-08-24 03:34:40 -0700

    update on add

--- opcode2.c
+++ opcode2.c
@@ -1,16 +1,22 @@
 #include "monty.h"
+
 /**
-  * _add - function adds top two elements of the stack
-  * @head: void
-  * @data: void
-  */
+ * _add - Function that adds the top two elements of the stack.
+ * @head: Double pointer to the head of the stack
+ * @line_number: Line number of the current opcode
+ */
 void _add(stack_t **head, unsigned int line_number)
 {
+	stack_t *temp;
+
 	if (*head == NULL || (*head)->next == NULL)
 	{
-		fprintf(stderr, "L%u: cant add, stack too short\n", line_number);
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-	(*head)->next->n += (*head)->n;
-	pop(head, line_number);
+	temp = *head;
+	(*head)->next->n += temp->n;
+	*head = (*head)->next;
+	(*head)->prev = NULL;
+	free(temp);
 }
